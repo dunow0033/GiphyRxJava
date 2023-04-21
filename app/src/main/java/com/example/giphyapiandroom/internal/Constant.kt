6eec1d473ba347626faa224335dd416728a5da65
Brian Donnoe <dunow33@hotmail.com>
2023-04-21 13:49:48 -0400

    started fresh from the tutor's github

---
+++ app/src/main/java/com/example/giphyapiandroom/internal/Constant.kt
@@ -0,0 +1,6 @@
+package com.example.giphyapiandroom.internal
+
+const val BASE_URL = "https://api.giphy.com/"
+const val DATABASE_NAME = "trending.db"
+const val LIMIT = "25"
+const val RATING = "g"
